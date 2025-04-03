5b9f86b087b605d1938780372b08c5d260983d0a
JuanNeftaly <00182222@uca.edu.sv>
2025-04-03 14:07:45 -0600

    Add: creating patient class :sparkles:

--- Labo1-N-Capas/src/model/Paciente.java
+++ Labo1-N-Capas/src/model/Paciente.java
@@ -1,4 +1,22 @@
 package model;
+import java.time.LocalDate;
 
-public class Paciente {
+public class Paciente extends Persona {
+
+    public Paciente(String name, String lastName, String dui, LocalDate birthdate) {
+        super(name, lastName, dui, birthdate);
+        if (isUnderage()) {
+            setDui("00000000-0");
+        }
+    }
+
+    @Override
+    public void setDui(String dui) {
+        if (isUnderage()) {
+            super.setDui("00000000-0");
+        } else {
+            super.setDui(dui);
+        }
+    }
 }
+
