1939759e9490bd26813186ff3cf0f475e8e5dd85
Gabriela <101780994+emptyGabriela@users.noreply.github.com>
2025-04-03 11:28:09 -0600

    creating person

--- Labo1-N-Capas/src/model/Persona.java
+++ Labo1-N-Capas/src/model/Persona.java
@@ -1,4 +1,55 @@
 package model;
 
+
+import java.time.LocalDate;
+
 public class Persona {
+    //nombre, apellido,dui, fecha de nacimiento
+    private String name;
+    private String lastName;
+    private String dui;
+    private LocalDate bithdate;
+
+    // constructor
+    public Persona(String name, String lastName, String dui, LocalDate bithdate) {
+        this.name = name;
+        this.lastName = lastName;
+        this.dui = dui;
+        this.bithdate = bithdate;
+    }
+
+    // Getters y Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDui() {
+        return dui;
+    }
+
+    public void setDui(String dui) {
+        this.dui = dui;
+    }
+
+    public LocalDate getBithdate() {
+        return bithdate;
+    }
+
+    public void setBithdate(LocalDate bithdate) {
+        this.bithdate = bithdate;
+    }
+
+
 }
