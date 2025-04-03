8dcc1068f9c811af2f2565c37921c866b58f31eb
JuanNeftaly <00182222@uca.edu.sv>
2025-04-03 14:26:32 -0600

    Add: creating doctor class :sparkles:

--- Labo1-N-Capas/src/model/Doctor.java
+++ Labo1-N-Capas/src/model/Doctor.java
@@ -1,4 +1,40 @@
 package model;
+import java.time.LocalDate;
 
-public class Doctor {
+// heredando de persona
+public class Doctor extends Persona {
+    private LocalDate fechaDeContratacion;
+    private String especialidad;
+    private String id;
+
+    public Doctor(String name, String lastName, LocalDate birthdate, LocalDate fechaDeContratacion, String especialidad, String id) {
+        super(name, lastName, "00000000-0", birthdate);
+        this.fechaDeContratacion = fechaDeContratacion;
+        this.especialidad = especialidad;
+        this.id = id;
+    }
+
+    public LocalDate getFechaDeContratacion() {
+        return fechaDeContratacion;
+    }
+
+    public void setFechaDeContratacion(LocalDate fechaDeContratacion) {
+        this.fechaDeContratacion = fechaDeContratacion;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
