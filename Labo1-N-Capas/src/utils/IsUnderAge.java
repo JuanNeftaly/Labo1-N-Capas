80d95e89639c6d9ff286abd3b2058239f849356f
JuanNeftaly <00182222@uca.edu.sv>
2025-04-04 17:31:54 -0600

    Add: create dummy data for patients :sparkles:

--- Labo1-N-Capas/src/utils/IsUnderAge.java
+++ Labo1-N-Capas/src/utils/IsUnderAge.java
@@ -1,7 +1,5 @@
 package utils;
-
 import model.Paciente;
-
 import java.time.LocalDate;
 
 public class IsUnderAge {
@@ -9,6 +7,7 @@ public class IsUnderAge {
         return LocalDate.now().minusYears(18).isBefore(birthdate);
     }
 
+    // verificamos si el chaval es mayor de edad, sino pal suelo el DUI
     public static void setDuiIfUnderage(Paciente paciente, String dui) {
         if (isUnderage(paciente.getBithdate())) {
             paciente.setDui("00000000-0");
