fbbdfd53ff52b7d064652c71a4a7fb00a858e0c2
JuanNeftaly <00182222@uca.edu.sv>
2025-04-03 22:10:24 -0600

    Add: Doctor generator ID :sparkles:

--- Labo1-N-Capas/src/model/Paciente.java
+++ Labo1-N-Capas/src/model/Paciente.java
@@ -1,24 +1,17 @@
 package model;
-import java.time.LocalDate;
 
-// herencia de persona
+import java.time.LocalDate;
+import utils.IsUnderAge;
+
 public class Paciente extends Persona {
 
     public Paciente(String name, String lastName, String dui, LocalDate birthdate) {
         super(name, lastName, dui, birthdate);
-        if (isUnderage()) {
-            setDui("00000000-0");
-        }
+        IsUnderAge.setDuiIfUnderage(this, dui);
     }
 
-    // metodo para verificar edad
     @Override
     public void setDui(String dui) {
-        if (isUnderage()) {
-            super.setDui("00000000-0");
-        } else {
-            super.setDui(dui);
-        }
+        IsUnderAge.setDuiIfUnderage(this, dui);
     }
 }
-
--- Labo1-N-Capas/src/utils/IsUnderAge.java
+++ Labo1-N-Capas/src/utils/IsUnderAge.java
@@ -6,4 +6,12 @@ public class IsUnderAge {
     public static boolean isUnderage(LocalDate birthdate) {
         return LocalDate.now().minusYears(18).isBefore(birthdate);
     }
+
+    public static void setDuiIfUnderage(Paciente paciente, String dui) {
+        if (isUnderage(paciente.getBithdate())) {
+            paciente.setDui("00000000-0");
+        } else {
+            paciente.setDui(dui);
+        }
+    }
 }
