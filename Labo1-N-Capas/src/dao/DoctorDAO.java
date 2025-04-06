8f7a3d8ecfeb49489eed0a87d1a936633f12bcef
Gabriela <101780994+emptyGabriela@users.noreply.github.com>
2025-04-05 23:16:48 -0600

    adding dao for doctor

--- Labo1-N-Capas/src/dao/DoctorDAO.java
+++ Labo1-N-Capas/src/dao/DoctorDAO.java
@@ -1,4 +1,27 @@
 package dao;
 
+import model.Doctor;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DoctorDAO {
+    private static final List<Doctor> doctores = new ArrayList<>();
+
+    public void addDoctor(Doctor doctor) {
+        doctores.add(doctor);
+    }
+
+    public List<Doctor> getAllDoctors() {
+        return doctores;
+    }
+
+    public Doctor getDoctorById(String id) {
+        for (Doctor d : doctores) {
+            if (d.getId().equals(id)) {
+                return d;
+            }
+        }
+        return null;
+    }
 }
+
