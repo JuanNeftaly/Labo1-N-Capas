2440678a17de21081134a5b9eb4478a56821548e
Gabriela <101780994+emptyGabriela@users.noreply.github.com>
2025-04-05 23:04:54 -0600

    adding all the appointment functionality ✨

--- Labo1-N-Capas/src/dao/CitaDAO.java
+++ Labo1-N-Capas/src/dao/CitaDAO.java
@@ -1,4 +1,22 @@
 package dao;
 
+import model.Cita;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CitaDAO {
+    private static final List<Cita> citas = new ArrayList<>();
+
+    public void addCita(Cita cita) {
+        citas.add(cita);
+    }
+
+    public List<Cita> getAllCitas() {
+        return citas;
+    }
+
+    public void removeCita(Cita cita) {
+        citas.remove(cita);
+    }
 }
+
--- Labo1-N-Capas/src/service/CitaService.java
+++ Labo1-N-Capas/src/service/CitaService.java
@@ -1,4 +1,60 @@
 package service;
 
+import dao.CitaDAO;
+import model.Cita;
+import model.Doctor;
+import model.Paciente;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
 public class CitaService {
+    private final CitaDAO citaDAO;
+
+    public CitaService() {
+        this.citaDAO = new CitaDAO();
+    }
+
+    public boolean scheduleCita(Doctor doctor, Paciente paciente, LocalDate fecha, LocalTime hora, boolean citaDelDia) {
+        if (!citaDelDia) {
+            if (hora.isBefore(LocalTime.of(8, 0)) || hora.isAfter(LocalTime.of(16, 0))) {
+                System.out.println("La hora debe ser entre 8:00 y 16:00 para citas futuras.");
+                return false;
+            } else {
+                fecha = LocalDate.now();
+            }
+            for (Cita c : citaDAO.getAllCitas()) {
+                if (c.getFecha().equals(fecha) && c.getHora().equals(hora)) {
+                    if (c.getDoctor().getId().equals(doctor.getId())) {
+                        System.out.println("El doctor ya tiene una Cita a esa hora.");
+                        return false;
+                    }
+                    if (c.getPaciente().getDui().equals(paciente.getDui())) {
+                        System.out.println("El paciente ya tiene una Cita a esa hora.");
+                        return false;
+                    }
+                }
+            }
+
+        }
+        Cita nuevaCita = new Cita(doctor, paciente, fecha, hora, citaDelDia);
+        citaDAO.addCita(nuevaCita);
+        return true;
+    }
+
+    public void cancelCita(Cita cita) {
+        citaDAO.removeCita(cita);
+    }
+
+    public List<Cita> listAllCitas() {
+        return citaDAO.getAllCitas();
+    }
+
+    public void listCitasByDoctorId(String doctorId) {
+        for (Cita c : citaDAO.getAllCitas()) {
+            if (c.getDoctor().getId().equals(doctorId)) {
+                System.out.println(c);
+            }
+        }
+    }
 }
